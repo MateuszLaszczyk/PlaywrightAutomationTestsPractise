cf4970b51fa19dde7a4c09b955f08f694fd2c2cf
mlaszczyk <mlaszczyk@griddynamics.com>
2025-08-04 10:31:52 +0200

    CheckBox style test

---
+++ src/main/java/org/example/ListButton.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
+
+public class ListButton {
+    private final Page page;
+
+    public ListButton(Page page) {
+        this.page = page;
+    }
+
+    public void expandList (){
+        Locator expandList = page.locator(".rct-icon.rct-icon-expand-all");
+        expandList.click();
+
+        }
+    }
+
