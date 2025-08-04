01d804dffcf0acf31ab64b209622d95ece940a36
mlaszczyk <mlaszczyk@griddynamics.com>
2025-08-04 15:46:32 +0200

    DropDownPage test implementation + added allure dependency

---
+++ src/main/java/org/example/DropDownPage.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import com.microsoft.playwright.Page;
+
+public class DropDownPage {
+    private final Page page;
+
+    public DropDownPage(Page page) {
+        this.page = page;
+    }
+
+    // Setters
+    public void selectOptionOne() {
+        page.selectOption("#dropdown", "1");
+    }
+
+    public void selectElementsPerPage() {
+        page.selectOption("#elementsPerPageSelect", "50");
+    }
+
+    public void selectCountry() {
+        page.selectOption("#country", "Poland");
+    }
+
+    // Getters
+    public String getSelectedDropdownValue() {
+        return page.locator("#dropdown").inputValue();
+    }
+
+    public String getSelectedElementsPerPageValue() {
+        return page.locator("#elementsPerPageSelect").inputValue();
+    }
+
+    public String getSelectedCountryValue() {
+        return page.locator("#country").inputValue();
+    }
+}
