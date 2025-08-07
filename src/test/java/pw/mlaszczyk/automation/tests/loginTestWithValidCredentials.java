2d993f66c9e9f26f03e6eeeae9a8ad1669adee64
MateuszLaszczyk <mati225511@gmail.com>
2025-08-07 18:16:41 +0200

    added login with invalid credentials test

--- src/test/java/pw/mlaszczyk/automation/tests/loginTestWithValidCredentials.java
+++ src/test/java/pw/mlaszczyk/automation/tests/loginTestWithValidCredentials.java
@@ -59,22 +59,6 @@ public class loginTestWithValidCredentials {
         loginPage = new loginPage(page);
     }
 
-
-    @DisplayName("""
-            Feature: Login with valid credentials
-            
-              Scenario: Login with valid credentials
-                Given User has valid credentials
-                When User navigates to initial page
-                  | https://www.saucedemo.com/ |
-                Then Following fields should be visible:
-                  | UsernameField  |
-                  | PasswordField  |
-                  | 'Login' button |
-                When User provides valid credentials into fields and clicks on 'Login' button
-                Then User should get logged in and product page should be displayed
-            """)
-
     @Test
     void shouldLoginWithValidCredentials() {
         loginPage.loginAndVerify(username, password, mainPageUrl);
