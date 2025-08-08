260939e6f5edfc9165dad11f778d913ea329daa9
MateuszLaszczyk <mati225511@gmail.com>
2025-08-08 14:38:58 +0200

    Change in config loader for jenkins to be able to get credentials from ENV

--- src/test/java/pw/mlaszczyk/automation/config/configLoader.java
+++ src/test/java/pw/mlaszczyk/automation/config/configLoader.java
@@ -4,42 +4,28 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
-/**
- * Utility class for loading configuration properties from an external file.
- * <p>
- * This class reads a properties file (typically `config.properties`) located in the
- * test resources directory and provides static access to its key-value pairs.
- *
- * Usage example:
- * <pre>
- *     String username = ConfigLoader.get("saucedemo.username");
- *     String password = ConfigLoader.get("saucedemo.password");
- *     String baseURL = ConfigLoader.get("saucedemo.baseUrl");
- *     String mainPageUrl = ConfigLoader.get("saucedemo.mainPageUrl");
- *     String invalidUerName = ConfigLoader.get("saucedemo.invalidusername");
- * </pre>
- */
 public class configLoader {
     private static final String CONFIG_PATH = "src/test/resources/config.properties";
-    private static Properties properties;
+    private static Properties properties = new Properties();
+    private static boolean isFileLoaded = false;
 
-    // Static initializer block to load properties only once
-    static {
-        properties = new Properties();
-        try (FileInputStream fis = new FileInputStream(CONFIG_PATH)) {
-            properties.load(fis);
-        } catch (IOException e) {
-            throw new RuntimeException("Unable to load configuration from: " + CONFIG_PATH, e);
-        }
-    }
-
-    /**
-     * Retrieves a property value from the configuration file by key.
-     *
-     * @param key the name of the property to retrieve
-     * @return the value associated with the given key, or null if the key doesn't exist
-     */
     public static String get(String key) {
+        // 1. First try to get from EVN
+        String envValue = System.getenv(key);
+        if (envValue != null && !envValue.isEmpty()) {
+            return envValue;
+        }
+
+        // 2. If env doesn't exist, use local properties
+        if (!isFileLoaded) {
+            try (FileInputStream fis = new FileInputStream(CONFIG_PATH)) {
+                properties.load(fis);
+                isFileLoaded = true;
+            } catch (IOException e) {
+                throw new RuntimeException("Unable to load configuration from: " + CONFIG_PATH, e);
+            }
+        }
+
         return properties.getProperty(key);
     }
 }
